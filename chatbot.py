291c9ea3dfeacc2a2fa4b1a99a779ff67db73597
Yuxian <xxx4gd@gmail.com>
2026-02-05 18:13:59 +0800

    Lab 3: Integrate ChatGPT API to Telegram Bot

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,10 @@ This program requires the following modules:
 - python-telegram-bot==22.5
 - urllib3==2.6.2
 '''
+# Import AI API
+from ChatGPT_HKBU import ChatGPT
+gpt = None
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 import configparser
@@ -18,6 +22,10 @@ def main():
     config = configparser.ConfigParser()
     config.read('config.ini')
 
+    # Create a ChatGPT client object
+    global gpt
+    gpt = ChatGPT(config)
+
     # Create an Application for your bot
     logging.info('INIT: Connecting the Telegram bot...')
     app = ApplicationBuilder().token(config['TELEGRAM']['ACCESS_TOKEN']).build()
@@ -31,11 +39,15 @@ def main():
     app.run_polling()
 
 async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # await update.message.reply_text(response)
     logging.info("UPDATE: " + str(update))
+    loading_message = await update.message.reply_text('Thinking...')
 
-    # send the echo back to the client
-    text = update.message.text.upper()
-    await update.message.reply_text(text)
+    # send the user message to the ChatGPT client
+    response = gpt.submit(update.message.text)
+
+    # send the response to the Telegram box client
+    await loading_message.edit_text(response)
 
 if __name__ == '__main__':
     main()
